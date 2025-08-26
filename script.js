50aa1ffae0cf95e09de73da0e041ee7d465aef24
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-26 13:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+for(let i=1; i<=5; i++){
+	let p = new Promise((resolve) => {
+		let delay = Math.floor(Math.random() * 5 + 1) * 1000;
+		setTimeout(()=> {
+			resolve(`Promise ${i} resolved after ${delay/1000}seconds`);
+		}, delay)
+	});
+	
+	window.promises.push(p)
+}
+
+Promise.any(window.promises)
+	.then(result => {
+		document.getElementById("output").textContent = result;
+	})
+	.catch(error => {
+		document.getElementById("output").textContent = "All Promises Rejected: " + error;
+	});
